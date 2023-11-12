9f93fdd05209824e6923b3199ca2ccfd4b02d402
Wonkee Kim <kimwonky@gmail.com>
2023-11-12 15:27:44 -0500

    Fix generating kickables

--- kick-cat/Assets/KickCat/Scripts/GameManager.cs
+++ kick-cat/Assets/KickCat/Scripts/GameManager.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Vector2 _generateKickableObjectIntervalRange = new Vector2(1f, 7f);
+    [SerializeField] private Vector2 _generateKickableObjectIntervalRange = new Vector2(0.4f, 1.7f);
 
+    private float _interval = 0f;
     private float _lastTimeKickableObjectGenerated = 0f;
 
     private void Update()
     {
-        if (Time.time - _lastTimeKickableObjectGenerated > Random.Range(_generateKickableObjectIntervalRange.x, _generateKickableObjectIntervalRange.y))
+        if (Time.time - _lastTimeKickableObjectGenerated > _interval)
         {
             _lastTimeKickableObjectGenerated = Time.time;
+            _interval = Random.Range(_generateKickableObjectIntervalRange.x, _generateKickableObjectIntervalRange.y);
             KickablesManager.CreateKickableObject((KickableType)Random.Range(0, System.Enum.GetValues(typeof(KickableType)).Length));
         }
     }
